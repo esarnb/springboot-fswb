681fdd0d027000d9fe82f61908602e86ca1447e8
esarnb <esarnb@gmail.com>
2022-01-28 15:41:55 -0500

    Structure Commit

---
+++ src/main/java/com/sba/fswa/FswaApplication.java
@@ -0,0 +1,13 @@
+package com.sba.fswa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FswaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FswaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sba/fswa/FswaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sba.fswa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FswaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
