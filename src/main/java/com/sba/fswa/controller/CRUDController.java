d778263100ce877d087b2eba033ac7b4b7158475
esarnb <esarnb@gmail.com>
2022-01-29 18:18:54 -0500

    Crud structure for controller, imported javax persistence for Entity setup

---
+++ src/main/java/com/sba/fswa/controller/CRUDController.java
@@ -0,0 +1,37 @@
+package com.sba.fswa.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController // REST label
+@CrossOrigin(origins = "http://localhost:4200") // Angular Port - local testing
+@RequestMapping(path="/crud") // main api endpoint
+public class CRUDController {
+
+    @GetMapping(path="/")
+    public String getData() {
+
+        return "GET REQUESTED";
+    }
+
+    @PostMapping(path="/")
+    @ResponseStatus(HttpStatus.CREATED) // changing default 200 to 201 status
+    public String postData() {
+
+        return "POST REQUESTED";
+    }
+
+    @PutMapping(path="/")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public String putData() {
+
+        return "PUT REQUESTED";
+    }
+
+    @DeleteMapping(path="/")
+    @ResponseStatus(HttpStatus.OK)
+    public String deleteData() {
+
+        return "DELETE REQUESTED";
+    }
+}
---
+++ src/main/java/com/sba/fswa/entities/DataObj.java
@@ -0,0 +1,11 @@
+package com.sba.fswa.entities;
+import javax.persistence.Entity;
+
+@Entity
+public class DataObj {
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String avatar_url;
+    private Integer age;
+}
