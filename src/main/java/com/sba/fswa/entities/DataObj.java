f9d05c094ae261d16b0767f931af2edf8e35ac5e
esarnb <esarnb@gmail.com>
2022-02-21 13:35:32 -0500

    Adding getter and setter for user data obj

--- src/main/java/com/sba/fswa/entities/DataObj.java
+++ src/main/java/com/sba/fswa/entities/DataObj.java
@@ -3,9 +3,50 @@ import javax.persistence.Entity;
 
 @Entity
 public class DataObj {
+
     private Integer id;
     private String firstname;
     private String lastname;
     private String avatar_url;
     private Integer age;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getAvatar_url() {
+        return avatar_url;
+    }
+
+    public void setAvatar_url(String avatar_url) {
+        this.avatar_url = avatar_url;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
