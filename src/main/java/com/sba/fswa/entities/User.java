fa623f00e99f7467885d0ca0e7081ebd344dc2db
esarnb <esarnb@gmail.com>
2022-03-22 13:13:42 -0400

    ID is autogenerated in entity User

--- src/main/java/com/sba/fswa/entities/User.java
+++ src/main/java/com/sba/fswa/entities/User.java
@@ -2,6 +2,9 @@ package com.sba.fswa.entities;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @Entity
@@ -9,7 +12,10 @@ import javax.persistence.Entity;
 //@NoArgsConstructor
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id; // PK
+
     private String firstname; // NN
     private String lastname; // NN
     private String avatar;
